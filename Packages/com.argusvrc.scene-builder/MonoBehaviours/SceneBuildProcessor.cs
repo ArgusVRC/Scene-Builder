5469532cbfd345bbfbb811905fe2b55060b2614c
KitKat <katjastoroygard@gmail.com>
2024-04-01 22:06:30 +0200

    re-init

---
+++ Packages/com.argusvrc.scene-builder/MonoBehaviours/SceneBuildProcessor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SceneBuilder
+{
+    public abstract class SceneBuildProcessor : MonoBehaviour
+    {
+        public abstract void OnBuildScene(GameObject copiedScene);
+    }
+
+    public abstract class SceneBuildPostProcessor
+    {
+        public abstract void OnPostBuildScene();
+    }
+}
